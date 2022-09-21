5673f82260384edde291906e6035d502713866b2
hedi <hedi@DESKTOP-MG5UJB9>
2022-09-21 11:44:49 +0430

    Add project files.

---
+++ Abstact_Step5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstact_Step5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user=new User();
+            Console.WriteLine("Please Enter Your Name");
+            Console.WriteLine(user.HelloUser(Console.ReadLine()));
+            Console.WriteLine("Please Enter Your Family");
+            Console.WriteLine(user.FamilyUser(Console.ReadLine()));
+            Console.ReadKey();
+        }
+    }  
+    public abstract class MyClass
+    {
+        public string HelloUser(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+    public class User : MyClass
+    {
+        public string FamilyUser(string name)
+        {
+            return "Your Family is " + name;
+        }
+    }
+    
+}
